01468fbdafbfdb5d862e85b17b93d76d2e7fb061
Aleksandar Dakic <aleksandardakic16@gmail.com>
2023-01-22 22:17:06 +0100

    Initial impl for async fd

--- tests/file_descriptor_async_test.cpp
+++ tests/file_descriptor_async_test.cpp
@@ -1,2 +1,29 @@
-#include "fd/async/awaitable.hpp"
+#include <vector>
+
+#include "catch2/catch_all.hpp"
 #include "fd/async/file_descriptor.hpp"
+#include "fd/async/task.hpp"
+#include "fmt/core.h"
+
+const auto path = fmt::format("{}/{}", TEST_RESOURCE_DIR, "test_file.txt");
+
+fd::async::task<std::size_t> get_size()
+{
+    fd::async::file_descriptor fd{ path, O_RDONLY };
+    std::vector<char> vec{ 4 };
+
+    auto size = co_await fd.async_read(vec, 4);
+
+    co_return size;
+};
+
+TEST_CASE("Read only coroutine", "[file_descriptor_async]")
+{
+    auto t = get_size();
+
+    REQUIRE(!t.is_done());
+
+    REQUIRE_NOTHROW(t.resume());
+
+    REQUIRE(t.get() == 4);
+}
